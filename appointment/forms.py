af7c07698c796f9fcddba44c0bc387cdd8360fe8
ayushi-pandya <ayushi.inexture@gmail.com>
2022-06-27 19:27:44 +0530

    update feature

--- appointment/forms.py
+++ appointment/forms.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from django import forms
+
 from appointment.models import Appointments
 from users.models import Staff
 
@@ -7,8 +10,25 @@ class PatientAppointmentForm(forms.ModelForm):
     """
     for making appointment form
     """
+    time_slot_choices = []
+    time = datetime.now()
+    current_time = time.strftime("%H:%M:%S")
+    print(time, '-----------', time.hour)
+    print(current_time.split(':')[0], '+++++++')
+    query = Appointments.objects.all().values('timeslot')
+    y=query[0]
+    print(query)
+    for i in range(9, 20):
+        if i > int(current_time.split(':')[0]):
+            y=query[0]
+            if i in query:
+                print('nn')
 
-    timeslot = forms.CharField()
+            # time_slot_choices.append((i, i))
+            time_slot_choices.append((f"{i}:00", f"{i}:00"))
+    print(time_slot_choices)
+
+    timeslot = forms.ChoiceField(choices=time_slot_choices)
 
     class Meta:
         model = Appointments
@@ -17,9 +37,3 @@ class PatientAppointmentForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(PatientAppointmentForm, self).__init__(*args, **kwargs)
         self.fields['staff'].queryset = Staff.objects.filter(is_approve=True).filter(is_available=True)
-
-    # def __init__(self, user, *args, **kwargs):
-    #     super(PatientAppointmentForm, self).__init__(*args, **kwargs)
-    #     self.fields['timeslot'] = forms.CharField(max_length=200
-    #         # choices=[(o.id, str(o)) for o in Waypoint.objects.filter(user=user)]
-    #     )
