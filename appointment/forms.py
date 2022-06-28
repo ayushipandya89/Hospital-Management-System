330f659c048fd2aca6d98f109a2a9bb68eb61297
ayushi-pandya <ayushi.inexture@gmail.com>
2022-06-28 17:30:57 +0530

    update appointment feature with validations

--- appointment/forms.py
+++ appointment/forms.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from django import forms
+from django.core.exceptions import ValidationError
 
 from appointment.models import Appointments
 from users.models import Staff
@@ -10,6 +11,7 @@ class PatientAppointmentForm(forms.ModelForm):
     """
     for making appointment form
     """
+
     class Meta:
         model = Appointments
         fields = ['staff', 'date', 'disease']
@@ -17,15 +19,6 @@ class PatientAppointmentForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(PatientAppointmentForm, self).__init__(*args, **kwargs)
         self.fields['staff'].queryset = Staff.objects.filter(is_approve=True).filter(is_available=True)
-
-    # def clean(self):
-    #     cleaned_data = super().clean()
-    #     fetch_date = cleaned_data.get('date')
-    #     print(fetch_date)
-    #     fetch_timeslot = cleaned_data.get('timeslot')
-    #     print(fetch_timeslot)
-    #     query = Appointments.objects.all().values('date','timeslot')
-    #     print(query)
 
 
 class PatientTimeslotsUpdate(forms.ModelForm):
@@ -35,28 +28,29 @@ class PatientTimeslotsUpdate(forms.ModelForm):
 
     time_slot_choices = []
     date = datetime.now().date()
-    # print(date,'/////////')
-    # if
     time = datetime.now()
     current_time = time.strftime("%H:%M:%S")
-    # print(time, '-----------', time.hour)
-    # print(current_time.split(':')[0], '+++++++')
-    query = Appointments.objects.all().values('timeslot')
-    # print(query)
-    for i in range(9, 20):
-        if i > int(current_time.split(':')[0]):
+    date_query = Appointments.objects.last().date
+    if date_query == date:
+        for i in range(9, 20):
+            if i > int(current_time.split(':')[0]):
+                if i != 12:
+                    time_slot_choices.append((f"{i}:00", f"{i}:00"))
+    else:
+        for i in range(9, 20):
             if i != 12:
                 time_slot_choices.append((f"{i}:00", f"{i}:00"))
-    # print(time_slot_choices)
-    query = Appointments.objects.all().values('date','timeslot')
-    date_list = []
-    # for d in query:
-    #     for key in d.keys():
-    #         print(d[key])
-    #         date_list.append(d[key])
-    # print(date_list)
     timeslot = forms.ChoiceField(choices=time_slot_choices)
 
     class Meta:
         model = Appointments
         fields = ['timeslot']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        fetch_time = cleaned_data.get("timeslot")
+        date_query = Appointments.objects.last().date
+        staff_query = Appointments.objects.last().staff
+        time_query = Appointments.objects.all().filter(date=date_query).filter(timeslot=fetch_time).filter(staff=staff_query)
+        if time_query:
+            raise ValidationError('This slot is already booked please choose another slot')
