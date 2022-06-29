54d57dbb17807c62b4ed0c581ec93e0807d2afaf
ayushi-pandya <ayushi.inexture@gmail.com>
2022-06-29 12:16:34 +0530

    update appointment feature with validations of date and time

--- appointment/forms.py
+++ appointment/forms.py
@@ -1,8 +1,6 @@
 from datetime import datetime
-
 from django import forms
 from django.core.exceptions import ValidationError
-
 from appointment.models import Appointments
 from users.models import Staff
 
@@ -19,6 +17,39 @@ class PatientAppointmentForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(PatientAppointmentForm, self).__init__(*args, **kwargs)
         self.fields['staff'].queryset = Staff.objects.filter(is_approve=True).filter(is_available=True)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        fetch_date = cleaned_data.get("date")
+        fetch_time = Appointments.objects.filter(date=fetch_date).values('timeslot')
+        data = []
+        user_data = []
+        time_slot_choices = []
+        time = datetime.now()
+        date = datetime.now().date()
+        current_time = time.strftime("%H:%M:%S")
+        # for displaying the timeslot after the current time
+        for i in range(9, 20):
+            if i > int(current_time.split(':')[0]):
+                if i != 12:
+                    user_data.append(i)
+                    time_slot_choices.append(f"{i}:00")
+        available_time = []
+        if int(current_time.split(':')[0]) >= 19:
+            raise ValidationError('The Hospital is close for today please choose another date')
+        else:
+            if fetch_date == date:
+                # gathering the data of available choices
+                for i in fetch_time:
+                    for j in i.values():
+                        available_time.append(j)
+                # for comparing the available choices of timeslot
+                result = all(elem in available_time for elem in time_slot_choices)
+                if result:
+                    raise ValidationError('ALl slots are booked for today..Please choose another date')
+            else:
+                if len(data) == 10:
+                    raise ValidationError('All slots of this date are booked..Please choose another date')
 
 
 class PatientTimeslotsUpdate(forms.ModelForm):
@@ -51,6 +82,7 @@ class PatientTimeslotsUpdate(forms.ModelForm):
         fetch_time = cleaned_data.get("timeslot")
         date_query = Appointments.objects.last().date
         staff_query = Appointments.objects.last().staff
-        time_query = Appointments.objects.all().filter(date=date_query).filter(timeslot=fetch_time).filter(staff=staff_query)
+        time_query = Appointments.objects.all().filter(date=date_query).filter(timeslot=fetch_time).filter(
+            staff=staff_query)
         if time_query:
             raise ValidationError('This slot is already booked please choose another slot')
