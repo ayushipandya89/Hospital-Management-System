02e41df7f94319b8b2f7acb521fd5eb2511c28d6
ayushi-pandya <ayushi.inexture@gmail.com>
2022-07-01 19:09:39 +0530

    added duty in nurse

---
+++ nurse/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+
+from appointment.models import Admit
+from nurse.models import NurseDuty
+from users.models import Staff
+
+
+class DutyForm(forms.ModelForm):
+    """
+    used for creating form to assign the duty of nurses.
+    """
+
+    class Meta:
+        model = NurseDuty
+        fields = ['staff', 'patient']
+
+    def __init__(self, *args, **kwargs):
+        super(DutyForm, self).__init__(*args, **kwargs)
+        self.fields["staff"].queryset = Staff.objects.filter(speciality='Nurse').filter(is_approve=True).filter(
+            is_available=True)
+        self.fields["patient"].widget = forms.widgets.CheckboxSelectMultiple()
+        self.fields["patient"].queryset = Admit.objects.values_list('patient', flat=True)
+        self.fields["patient"].queryset = Admit.objects.all().filter(out_date__isnull=True)
+        # self.fields["patient"].queryset = Admit.objects.filter(out_date__isnull=True).values_list('pk', flat=True)
