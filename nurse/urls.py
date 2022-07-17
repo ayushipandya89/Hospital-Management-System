45bc5a54b7ee40e42a31b78936ae6601056eccd9
ayushi-pandya <ayushi.inexture@gmail.com>
2022-07-18 00:25:28 +0530

    add search functionality in listviews in nurse app

--- nurse/urls.py
+++ nurse/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import ViewDuty
+from .views import ViewDuty, SearchDuty
 
 urlpatterns = [
     path('duty/', views.AssignDuty.as_view(), name='duty'),
     path('view_duty/', ViewDuty.as_view(), name='view-duty'),
+    path('search_duty/', SearchDuty.as_view(), name='search_duty'),
 
 ]
--- nurse/views.py
+++ nurse/views.py
@@ -1,6 +1,8 @@
 from django.contrib.messages.views import SuccessMessageMixin
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic import CreateView, ListView
 
 from constants import DUTY_ASSIGN_MSG
@@ -26,13 +28,37 @@ class AssignDuty(SuccessMessageMixin, CreateView):
         return render(request, 'appointment/not_admin.html')
 
 
-class ViewDuty(ListView):
+class SearchDuty(View):
+    """
+    class for give data to ajax call for search user
+    """
+
+    def get(self, request):
+        room = NurseDuty.objects.all().values_list('staff__staff__username', flat=True)
+        room_list = list(room)
+        return JsonResponse(room_list, safe=False)
+
+
+class ViewDuty(View):
     """
     class for view the list of duty
     """
-    model = NurseDuty
-    template_name = 'nurse/view_duty.html'
-    context_object_name = 'nurseduty'
+
+    def get(self, request):
+        all_data = NurseDuty.objects.all()
+        context = {
+            'all_data': all_data
+        }
+        return render(request, 'nurse/view_duty.html', context)
+
+    def post(self, request):
+        search = request.POST['search']
+        if search != " ":
+            search = search.strip()
+            user = NurseDuty.objects.filter(staff__staff__username__icontains=search)
+            return render(request, 'nurse/view_duty.html', {'data': user})
+        else:
+            return redirect('Hospital-home')
 
     def dispatch(self, request, *args, **kwargs):
         if is_admin(user=self.request.user):
